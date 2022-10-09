36d451f71f9ca6df7e4463a7a77f005e941cb819
Abdlrhmn-Morsi <abdlrhmnmorsi@gmail.com>
2022-10-09 23:46:29 +0200

    v3

--- lib/const/const.dart
+++ lib/const/const.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String apiKey = '984005c37e8847f49f9ceaa50f3479b8';
+const String apiKey = 'ADD_YOUR_API_KEY';
 const String baseUrl = 'https://newsapi.org/v2/top-headlines?';
 const String language = 'language=en';
 const String country = 'country=us';
